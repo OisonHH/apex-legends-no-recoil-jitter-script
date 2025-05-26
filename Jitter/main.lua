15958e978da90b7ea4973d0401f672b867201643
Oison <edvin.weberg.spel@gmail.com>
2025-05-26 17:42:40 +0200

    Add files via upload

---
+++ Jitter/main.lua
@@ -0,0 +1,18 @@
+function OnEvent(event, arg)
+    if event == "PROFILE_ACTIVATED" then
+        EnablePrimaryMouseButtonEvents(true)
+    end
+
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
+        is_firing = true
+        if IsKeyLockOn("capslock") then
+            repeat
+                MoveMouseRelative(-6, 6)
+                Sleep(10)
+                MoveMouseRelative(6, -6)
+                Sleep(10)
+            until not IsMouseButtonPressed(1)
+        end
+        is_firing = false
+      end
+    end
